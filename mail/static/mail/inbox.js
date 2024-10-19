6c562d908572ad9b62b328a33ee6b9671021951e
unknown <eloissemolina@gmail.com>
2024-10-18 23:45:12 -0600

    changes

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -212,3 +212,30 @@ function archived_email(email, archived)
   }
   
 }
+
+let compose_form = document.getElementById('compose-form');
+let compose_recipients = document.getElementById('compose-recipients');
+let compose_subject = document.getElementById('compose-subject');
+let compose_body = document.getElementById('compose-body');
+
+compose_form.addEventListener('submit', (e) => {
+  e.preventDefault()
+    fetch('/emails', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                recipients: compose_recipients.value,
+                subject: compose_subject.value,
+                body: compose_body.value
+            }
+        )
+    })
+        .then(response => response.json())
+        .then(() => {
+            load_mailbox('sent')
+
+        })
+})
