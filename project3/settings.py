d4c29894ceccc57ec0b2550bd30d842f981050e0
unknown <eloissemolina@gmail.com>
2024-10-18 23:17:40 -0600

    configs for posgres

--- project3/settings.py
+++ project3/settings.py
@@ -83,7 +83,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+    MIGRATION_MODULES = {
+        'auth': None,
+        'contenttypes': None,
+        'sessions': None,
+        'admin': None,
+        'messages': None,
+    }
 AUTH_USER_MODEL = 'mail.User'
 
 # Password validation
